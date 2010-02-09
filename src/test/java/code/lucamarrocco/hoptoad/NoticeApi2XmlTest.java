b1a0d3da6a7775cdc47ae3a95ddd97bc85f637fb
Aaron Peckham <gowalla-android-dev@googlegroups.com>
2010-02-09 15:13:13 -0800

    test was removing quotes from string before assertions

--- src/test/java/code/lucamarrocco/hoptoad/NoticeApi2XmlTest.java
+++ src/test/java/code/lucamarrocco/hoptoad/NoticeApi2XmlTest.java
@@ -13,10 +13,6 @@ public class NoticeApi2XmlTest {
 
 	private HoptoadNotice notice;
 
-	private String clean(String string) {
-		return string.replaceAll("\\\"", "");
-	}
-
 	@SuppressWarnings({"ThrowableInstanceNeverThrown"})
   @Before
 	public void setUp() {
@@ -27,66 +23,66 @@ public class NoticeApi2XmlTest {
 
 	@Test
 	public void testApiKey() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<api-key>apiKey</api-key>"));
+		assertThat(xml(), containsString("<api-key>apiKey</api-key>"));
 	}
 
-	@Test
+  @Test
 	public void testError() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("error>"));
+		assertThat(xml(), containsString("error>"));
 	}
 
 	@Test
 	public void testErrorBacktrace() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("backtrace>"));
+		assertThat(xml(), containsString("backtrace>"));
 	}
 
 	@Test
 	public void testErrorBacktraceLine() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<line method=org.junit.internal.runners.MethodRoadie.run file=MethodRoadie.java number=42/>"));
+		assertThat(xml(), containsString("<line method=\"org.junit.internal.runners.MethodRoadie.run\" file=\"MethodRoadie.java\" number=\"42\"/>"));
 	}
 
 	@Test
 	public void testErrorClass() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<class>java.lang.RuntimeException</class>"));
+		assertThat(xml(), containsString("<class>java.lang.RuntimeException</class>"));
 	}
 
 	@Test
 	public void testErrorMessage() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<message>errorMessage</message>"));
+		assertThat(xml(), containsString("<message>errorMessage</message>"));
 	}
 
 	@Test
 	public void testNoticeVersion() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("notice version=2.0"));
+		assertThat(xml(), containsString("notice version=\"2.0.0\""));
 	}
 
 	@Test
 	public void testNotifier() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("notifier>"));
+		assertThat(xml(), containsString("notifier>"));
 	}
 	
 	@Test
 	public void testNotifierName() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<name>hoptoad</name>"));
+		assertThat(xml(), containsString("<name>hoptoad</name>"));
 	}
 	
 	@Test
 	public void testNotifierUrl() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<url>http://hoptoad.googlecode.com</url>"));
+		assertThat(xml(), containsString("<url>http://hoptoad.googlecode.com</url>"));
 	}
 
 	@Test
 	public void testNotifierVersion() {
-		assertThat(xml(new NoticeApi2(notice)), containsString("<version>1.7-socrata-SNAPSHOT</version>"));
+		assertThat(xml(), containsString("<version>1.7-socrata-SNAPSHOT</version>"));
 	}
 
   @Test
   public void testEscapesAngleBrackets() throws Exception {
-    String actual = xml(new NoticeApi2(notice));
+    String actual = xml();
     assertThat(actual, containsString("&lt;blink&gt;production&lt;/blink&gt;"));
   }
 
-	private String xml(NoticeApi2 noticeApi) {
-		return clean(noticeApi.toString());
-	}
+  private String xml() {
+    return new NoticeApi2(notice).toString();
+  }
 }
